bd9eb648089a8ccb946063b48685dd517aa2e7d5
31persson <jperscas@myuax.com>
2024-02-07 13:01:36 +0100

    ejercicio 2

---
+++ ejercicio2.java
@@ -0,0 +1,16 @@
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+        // Mayor número representable en char
+        char mayorChar = Character.MAX_VALUE;
+        System.out.println("Mayor número representable en char: " + (int) mayorChar);
+
+        // Mayor número representable en short
+        short mayorShort = Short.MAX_VALUE;
+        System.out.println("Mayor número representable en short: " + mayorShort);
+
+        // Mayor número representable en int
+        int mayorInt = Integer.MAX_VALUE;
+        System.out.println("Mayor número representable en int: " + mayorInt);
+    }
+}
